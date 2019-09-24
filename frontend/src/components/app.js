3a457d59c6ba31f1feb8174358361bb8e6ae3ecd
Nicolai <organ.nicolai@gmail.com>
2019-09-24 09:21:02 +0300

    check if loged

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import WelcomeComponent from './pages/welcome';
+import axios from "axios";
 import Home from './pages/home';
 import SignIn from './auth/signIn';
 import SignUp from './auth/signUp';
@@ -10,6 +11,76 @@ import {
  } from "react-router-dom";
  
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loggedInStatus: "NOT_LOGGED_IN",
+      userdata:""
+    };
+
+    this.handleSuccessfulLogin = this.handleSuccessfulLogin.bind(this);
+    this.handleUnsuccessfulLogin = this.handleUnsuccessfulLogin.bind(this);
+    this.handleSuccessfulLogout = this.handleSuccessfulLogout.bind(this)
+  }
+  handleSuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "LOGGED_IN"
+    });
+  }
+
+  handleSuccessfulLogout() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+
+  handleUnsuccessfulLogin() {
+    this.setState({
+      loggedInStatus: "NOT_LOGGED_IN"
+    });
+  }
+  checkLoginStatus() {
+    
+    let token = localStorage.getItem('token');
+    let data = {headers: {
+        Accept:'application/json',
+        Authorization:`Bearer ${token}`
+        }};
+    return axios
+      .get("http://localhost:10077/api/user",data)
+      .then(response => {
+        const loggedIn = response.data.loggedIn;
+        console.log(response);
+        const loggedInStatus = this.state.loggedInStatus;
+        // If loggedIn and status LOGGED_IN => return data
+        // If loggedIn status NOT_LOGGED_IN => update state
+        // If not loggedIn and status LOGGED_IN => update state
+        console.log('loggedIn',loggedIn);
+        console.log('loggedInStatus', loggedInStatus)
+        if (loggedIn && loggedInStatus === "LOGGED_IN") {
+          console.log('cond1');
+          return loggedIn;
+        } else if (loggedIn && loggedInStatus === "NOT_LOGGED_IN") {
+          console.log('cond2');
+          this.setState({
+            loggedInStatus: "LOGGED_IN",
+            userdata: response.data
+          });
+        } else if (!loggedIn && loggedInStatus === "LOGGED_IN") {
+          console.log('cond3');
+          this.setState({
+            loggedInStatus: "NOT_LOGGED_IN",
+            userdata:''
+          });
+        }
+      })
+      .catch(error => {
+        console.log("Error", error);
+      });
+  }
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
   render() {
     return (
       <div className='app'>
