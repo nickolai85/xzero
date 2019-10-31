221c1d0e7fdcf4a5260eb27a2c7f0b58f34ca814
nickolai85 <organ.nicolai@gmail.com>
2019-10-31 18:12:50 +0200

    init game components

--- frontend/src/components/pages/home.js
+++ frontend/src/components/pages/home.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Chat from './chat';
 import Auth from './auth';
+import Game from '../game/game';
 export default class Home extends Component {
 
    render() {
      return (
        <div>
-           {this.props.loggedInStatus ? <Chat/> : <Auth/>}
+           {this.props.loggedInStatus ? <Chat /> : <Auth />}
+            <div>
+                <Game />
+            </div>
        </div>
      );
    }
