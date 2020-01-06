ded7dda9b027f6e0eb1900c1b8ce64438cca2e97
nickolai85 <organ.nicolai@gmail.com>
2020-01-06 16:48:29 +0200

    Channell model

--- backend/app/Models/Channel.php
+++ backend/app/Models/Channel.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use  App\User;
 
 class Channel extends Model
 {
-    //
+    protected $fillable = [
+        'created_user', 'joined_user', 'game_id', 'code', 'status'
+    ];
+
+    public function gameCreator()
+    {
+        return $this->belongsTo(User::class, 'created_user');
+    }
+
+    public function joinedGame()
+    {
+        return $this->belongsTo(User::class, 'joined_user');
+    }
 }
